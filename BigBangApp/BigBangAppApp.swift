e8f1c4a78978ec7ee5ec3bab52eb4ddf12407723
Rubén <ruben.sr.apple@gmail.com>
2024-11-21 12:15:16 +0100

    Initial Commit

---
+++ BigBangApp/BigBangAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangAppApp.swift
+//  BigBangApp
+//
+//  Created by Rubén Segura Romo on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BigBangAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigBangApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BigBangApp
+//
+//  Created by Rubén Segura Romo on 21/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
