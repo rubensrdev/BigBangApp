de21e7dd7ce0a3abd9e668a759c3cdb681398795
Rubén <ruben.sr.apple@gmail.com>
2024-11-29 11:32:52 +0100

    Documentando el proyecto

--- BigBangApp/ViewModel/BigBangViewModel.swift
+++ BigBangApp/ViewModel/BigBangViewModel.swift
@@ -7,9 +7,36 @@
 
 import Foundation
 
+/// Un modelo de vista para gestionar la lógica de los episodios en la aplicación
+/// ``BigBangViewModel`` actua como fuente de la verdad para los episodios de la serie, gestionando la interacción
+/// entre el repositorio de datos y las vistas como un objeto global de la aplicación.
+/// ### Temas
+/// - Propiedades:
+///   - ``repository``
+///   - ``episodes``
+///   - ``seasonSelected``
+///   - ``searchText``
+///   - ``filteredEpisodes``
+///   - ``favoriteEpisodes``
+/// - Métodos:
+///   - ``updateFilteredEpisodes()``
+///   - ``update(_:)``
+///   - ``updateFavoriteEpisodes()``
+///
+/// ### Dependencias
+/// - Basado en ``RepositoryProtocol`` para cargar y guardar datos de episodios.
+/// - Utiliza la enumeración ``OrderBySeason`` para definir la temporada seleccionada.
+///
 final class BigBangViewModel: ObservableObject {
+    /// El repositorio gestiona las operaciones de carga y guardadode datos
+    ///
+    /// La dependencia se inyecta en el inicializador, permitiendo distintas configuraciones
+    /// por ejemplo, datos de prueba para entornos de desarrollo
     let repository: RepositoryProtocol
     
+    /// La lista completa de episodios cargados y gestionados
+    ///
+    /// Cada vez que esta propiuedad cambia, los datos se guardan automáticamente en el repositorio
     @Published var episodes: Episodes {
         didSet {
             do {
@@ -19,20 +46,44 @@ final class BigBangViewModel: ObservableObject {
             }
         }
     }
+    
+    /// La temporada seleccionada para filtrar los episodios
+    ///
+    /// Las opciones son valores definidos en ``OrderBySeason``
+    /// Cuando cambia se actualizan los episodios filtrados automáticamente
     @Published var seasonSelected: OrderBySeason = .defaultSeason {
         didSet {
             updateFilteredEpisodes()
         }
     }
+    
+    /// El texto introducido para buscar episodios por su nombre
+    ///
+    /// Cuando cambia, los episodios filtrados se actualizan automáticamente
     @Published var searchText: String = "" {
         didSet {
             updateFilteredEpisodes()
         }
     }
+    
+    /// La lista de episodios filtrados por temporada y texto de búsqueda.
+    ///
+    /// Esta propiedad se actualiza dinámicamente según las opciones de filtrado actuales.
     @Published var filteredEpisodes: Episodes = []
+    
+    /// La lista de episodios marcados como favoritos.
+    ///
+    /// Esta propiedad se genera dinámicamente, mostrando solo los episodios que
+    /// tienen la propiedad ``favorite`` marcada.
     @Published var favoriteEpisodes: Episodes = []
     
-    
+    /// Inicializa el modelo de vista cargando datos desde el repositorio proporcionado.
+    ///
+    /// Si la lista de episodios no está vacía, se inicializan las propiedades relacionadas
+    /// con la temporada seleccionada y los episodios favoritos.
+    ///
+    /// - Parameter repository: El repositorio que gestiona las operaciones de datos. Por defecto,
+    ///   se utiliza una instancia de producción.
     init(repository: RepositoryProtocol = Repository()) {
         self.repository = repository
         do {
@@ -48,6 +99,9 @@ final class BigBangViewModel: ObservableObject {
         }
     }
     
+    /// Actuliza la lista de episodios filtrados según la temporada seleccionada y el texto de búsqueda
+    ///
+    /// Este método aplica los criterios de filtrado seleccionados y ordena los episodios por número
     func updateFilteredEpisodes() {
         filteredEpisodes = episodes.filter { episode in
             let matchesSeason = episode.episodeData.season == seasonSelected.rawValue
@@ -56,6 +110,12 @@ final class BigBangViewModel: ObservableObject {
         }.sorted { $0.episodeData.number < $1.episodeData.number }
     }
     
+    /// Actualiza los datos de un episodio existente con nuevos datos.
+    ///
+    /// Este método localiza un episodio en la lista principal usando su ID y reemplaza sus datos con los proporcionados.
+    /// También actualiza las listas de episodios fltrados y favoritos
+    ///
+    /// - Parameter episode: El episodio actualizado que debe reemplazar al existente
     func update(_ episode: Episode) {
         if let index = episodes.firstIndex(where: { $0.id == episode.id }) {
             episodes[index] = episode
@@ -64,6 +124,9 @@ final class BigBangViewModel: ObservableObject {
         }
     }
     
+    /// Actualiza la lista de episodios favoritos
+    ///
+    /// Este método filtra y ordena los episodios que tiene la propiedad ``favorite`` marcada 
     func updateFavoriteEpisodes() {
         favoriteEpisodes = episodes.filter { $0.favorite }
                                     .sorted { $0.episodeData.number < $1.episodeData.number }
--- BigBangApp/ViewModel/EpisodeEditViewModel.swift
+++ BigBangApp/ViewModel/EpisodeEditViewModel.swift
@@ -8,15 +8,45 @@
 import Foundation
 import SwiftUI
 
+/// Un modelo de vista para gestionar la lógica de edición de los detalles de un episodio ``Episode``
+///
+/// ``EpisodeEditViewModel`` gestiona el estado y lógica de edición de una instancia de ``Episode``
+/// Utiliza las propiedades publicadas `@Published` para permitir actulizaciones reactivas desde la vista
+///
+/// ### Temas
+/// - Propiedades:
+///   - ``isFavorite``
+///   - ``isWatched``
+///   - ``rating``
+///   - ``notes``
+/// - Métodos:
+///   - ``progressColor(for:)``
+///   - ``updateEpisodeControlItems()``
+///
+/// ### Dependencias
+///- Basado en el modelo ``Episode`` para proporcionar datos iniciales y generar un nuevo modelo actualizado
 final class EpisodeEditViewModel: ObservableObject {
     
+    /// El episodio original que se está editando
     var episode: Episode
     
+    /// Indica si el episodio está marcado como favorito
     @Published var isFavorite: Bool = false
+    
+    /// Indica si el episodio está marcado como visto
     @Published var isWatched: Bool = false
+    
+    /// Indica si el episodio está puntuado
     @Published var rating: Int = 0
+    
+    /// Indica si el episodio tiene notas
     @Published var notes: String = ""
     
+    /// Inicializa el modelo de vista con un episodio específico.
+    ///
+    /// Este método toma un ``Episode`` y convierte sus propiedades en valores editables.
+    ///
+    /// - Parameter episode: El episodio que se desea editar.
     init(episode: Episode) {
         self.episode = episode
         self.isFavorite = episode.favorite
@@ -25,6 +55,13 @@ final class EpisodeEditViewModel: ObservableObject {
         self.notes = episode.notes
     }
     
+    /// Proporciona un color representativo basado en la puntuación del episodio.
+    ///
+    /// Este método traduce la puntuación de un episodio en un color visual para
+    /// mejorar la representación de la calidad percibida del episodio.
+    ///
+    /// - Parameter rating: La puntuación del episodio, de 0 a 5.
+    /// - Returns: Un color que representa la calidad del episodio.
     func progressColor(for rating: Int) -> Color {
         switch rating {
         case 0...1:
@@ -42,6 +79,12 @@ final class EpisodeEditViewModel: ObservableObject {
         }
     }
     
+    /// Actualiza los valores editados en el modelo ``Episode`` y devuelve un modelo actualizado.
+    ///
+    /// Este método aplica las modificaciones realizadas en las propiedades editables al
+    /// modelo ``Episode`` y genera una nueva instancia con esos valores actualizados.
+    ///
+    /// - Returns: Una nueva instancia de ``Episode`` con los valores actualizados.
     func updateEpisodeControlItems() -> Episode {
         episode.favorite = isFavorite
         episode.watched = isWatched
